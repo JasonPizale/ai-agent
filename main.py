17faa624876f723fe8fecc39a01ab0bb56dd92cf
JasonPizale <jasonpizale@gmail.com>
2025-09-04 13:29:33 -0400

    created a list to store info, cleaned up code

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+from google.genai import types 
 
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        print("error: GEMINI_API_KEY not set")
+        sys.exit(1)
+
+    client = genai.Client(api_key=api_key)
+
     if len(sys.argv) < 2:
         print("error: no prompt provided")
         sys.exit(1)
     else:
-        args = sys.argv[1:]
-        prompt = " ".join(args)
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+        prompt = " ".join(sys.args[1:])
+        messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=messages)
+        
         print("Response:")
         print(response.text)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
