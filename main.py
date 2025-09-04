5822581270204ba092682e505b46d2ab27bfc1a0
JasonPizale <jasonpizale@gmail.com>
2025-09-04 13:15:17 -0400

    working on user prompts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,10 +7,15 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) < 2:
+        print("error: no prompt provided")
+        sys.exit(1)
+    else:
+        args = sys.argv[1:]
+        prompt = " ".join(args)
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
