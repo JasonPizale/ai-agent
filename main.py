a76df332f0ed489f1e6e47511b2948065c272cef
JasonPizale <jasonpizale@gmail.com>
2025-09-04 13:18:21 -0400

    working on user prompts

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def main():
     else:
         args = sys.argv[1:]
         prompt = " ".join(args)
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt
+        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+        print("Response:")
+        print(response.text)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
