b97d5a2d31542294fdf71a0f84e208f823a3462e
JasonPizale <jasonpizale@gmail.com>
2025-09-08 17:19:08 -0400

    Implement get_file_content and update tests

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    abs_working_directory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(file_path)
+
+    if not abs_file_path.startswith(abs_working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(abs_file_path, "r") as f:
+            file_content = f.read()
+
+        if len(file_content) > MAX_CHARS:
+            file_content = file_content[:MAX_CHARS]
+            file_content = file_content + f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+    except Exception as e:
+        return f"Error: {e}"
+
+    return file_content
