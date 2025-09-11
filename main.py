469f273bc82ac9ee9d60222858ae3513d7237769
JasonPizale <jasonpizale@gmail.com>
2025-09-11 17:28:41 -0400

    Implement system prompt configuration for AI model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types 
 
 def main():
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT"\"
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -25,7 +26,9 @@ def main():
 
         response = client.models.generate_content(
             model="gemini-2.0-flash-001", 
-            contents=messages)
+            contents=messages,
+            config=types.GenerateContentConfig(system_instruction=system_prompt),
+            )
         
         print("Response:")
         print(response.text)
