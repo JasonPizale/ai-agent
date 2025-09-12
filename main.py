05e77f96463dc5915f314961042688d42ae0157f
JasonPizale <jasonpizale@gmail.com>
2025-09-12 13:48:42 -0400

    Add LLM tool declaration and response handling

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
-from google.genai import types 
+from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def main():
-    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT"\
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -25,14 +34,28 @@ def main():
 
         messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
+
+        available_functions = types.Tool(
+            function_declarations=[
+                schema_get_files_info,
+            ]
+            )
+        
         response = client.models.generate_content(
             model="gemini-2.0-flash-001", 
             contents=messages,
-            config=types.GenerateContentConfig(system_instruction=system_prompt),
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+                )
             )
         
         print("Response:")
-        print(response.text)
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        else:
+            print(response.text)
+            
         if verbose:
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
